d80ec1f8a358cbc5bbcd7939103890e1f4cae62c
piupuer <piupuer@gmail.com>
2022-10-16 14:23:22 +0800

    user group can edit users/action

--- src/pages/system/user-group/index.vue
+++ src/pages/system/user-group/index.vue
@@ -87,6 +87,58 @@
           <t-form-item label="关键字" name="word">
             <t-input v-model="editFormData.word" placeholder="请输入内容" />
           </t-form-item>
+          <t-form-item v-if="isEdit" label="用户列表" name="users">
+            <t-select-input
+              v-model:inputValue="selectUserInput"
+              :value="selectUserData"
+              :allow-input="true"
+              placeholder="请选择或输入"
+              :tag-input-props="{ excessTagsDisplayType: 'break-line' }"
+              clearable
+              multiple
+              @tag-change="selectUserTagChange"
+              @input-change="selectUserInputChange"
+            >
+              <template #panel>
+                <t-checkbox-group
+                  v-if="selectUserOptions.length"
+                  v-model="selectUserChecked"
+                  :options="selectUserOptions"
+                  @change="selectUserOptionsCheckedChange"
+                />
+                <div v-else class="tdesign-demo__select-empty-multiple">暂无数据</div>
+              </template>
+              <template #suffixIcon>
+                <t-icon class="chevron-down" />
+              </template>
+            </t-select-input>
+          </t-form-item>
+          <t-form-item v-if="isEdit" label="授权行为" name="action">
+            <t-select-input
+              v-model:inputValue="selectActionInput"
+              :value="selectActionData"
+              :allow-input="true"
+              placeholder="请选择或输入"
+              :tag-input-props="{ excessTagsDisplayType: 'break-line' }"
+              clearable
+              multiple
+              @tag-change="selectActionTagChange"
+              @input-change="selectActionInputChange"
+            >
+              <template #panel>
+                <t-checkbox-group
+                  v-if="selectActionOptions.length"
+                  v-model="selectActionChecked"
+                  :options="selectActionOptions"
+                  @change="selectActionOptionsCheckedChange"
+                />
+                <div v-else class="tdesign-demo__select-empty-multiple">暂无数据</div>
+              </template>
+              <template #suffixIcon>
+                <t-icon class="chevron-down" />
+              </template>
+            </t-select-input>
+          </t-form-item>
         </t-form>
       </t-dialog>
     </div>
@@ -98,6 +150,8 @@ import { MessagePlugin } from 'tdesign-vue-next';
 import { useSettingStore } from '@/store';
 import { prefix } from '@/config/global';
 import { createUserGroup, deleteUserGroup, findUserGroup, updateUserGroup } from '@/api/userGroup';
+import { findUser } from '@/api/user';
+import { findAction } from '@/api/action';
 
 const store = useSettingStore();
 
@@ -105,9 +159,9 @@ const rowKey = 'id';
 const verticalAlign = 'top';
 const hover = true;
 const pagination = ref({
-  defaultPageSize: 20,
-  total: 100,
-  defaultCurrent: 1,
+  current: 1,
+  pageSize: 20,
+  total: 0,
 });
 const COLUMNS = [
   {
@@ -166,21 +220,65 @@ const fetchData = async () => {
   dataLoading.value = true;
   const params = {
     ...formData.value,
-    'page.num': pagination.value.defaultCurrent,
-    'page.size': pagination.value.defaultPageSize,
+    'page.num': pagination.value.current,
+    'page.size': pagination.value.pageSize,
   };
   try {
     const { list, page } = await findUserGroup(params);
     data.value = list;
     pagination.value = {
-      defaultCurrent: Number(page.num),
-      defaultPageSize: Number(page.size),
+      current: Number(page.num),
+      pageSize: Number(page.size),
       total: Number(page.total),
     };
   } catch (e) {
     console.log(e);
   } finally {
     dataLoading.value = false;
+  }
+};
+
+const fetchUserData = async () => {
+  const params = {
+    username: selectUserInput.value,
+    'page.num': pagination.value.current,
+    'page.size': pagination.value.pageSize,
+  };
+  try {
+    const { list } = await findUser(params);
+    const arr = [];
+    for (const k in list) {
+      arr.push({
+        label: `${list[k].username}[${list[k].code}]`,
+        value: list[k].id,
+      });
+    }
+    selectUserOptions.value = arr;
+  } catch (e) {
+    console.log(e);
+  } finally {
+  }
+};
+
+const fetchActionData = async () => {
+  const params = {
+    word: selectActionInput.value,
+    'page.num': pagination.value.current,
+    'page.size': pagination.value.pageSize,
+  };
+  try {
+    const { list } = await findAction(params);
+    const arr = [];
+    for (const k in list) {
+      arr.push({
+        label: `${list[k].word}[${list[k].name}]`,
+        value: list[k].code,
+      });
+    }
+    selectActionOptions.value = arr;
+  } catch (e) {
+    console.log(e);
+  } finally {
   }
 };
 
@@ -202,6 +300,24 @@ const showEdit = (row) => {
   if (row) {
     editFormData.value = row;
     editHeader.value = `编辑${row.id}`;
+    const arr1 = [];
+    for (const k in row.users) {
+      arr1.push({
+        label: `${row.users[k].username}[${row.users[k].code}]`,
+        value: row.users[k].id,
+      });
+    }
+    selectUserData.value = arr1;
+    selectUserCheckedChange();
+    const arr2 = [];
+    for (const k in row.actions) {
+      arr2.push({
+        label: `${row.actions[k].word}[${row.actions[k].name}]`,
+        value: row.actions[k].code,
+      });
+    }
+    selectActionData.value = arr2;
+    selectActionCheckedChange();
   } else {
     editHeader.value = '新增';
   }
@@ -229,7 +345,19 @@ const doCreate = async () => {
 
 const doEdit = async () => {
   try {
-    await updateUserGroup(editFormData.value);
+    const arr1 = [];
+    for (const k in selectUserData.value) {
+      arr1.push(selectUserData.value[k].value);
+    }
+    const arr2 = [];
+    for (const k in selectActionData.value) {
+      arr2.push(selectActionData.value[k].value);
+    }
+    await updateUserGroup({
+      ...editFormData.value,
+      users: arr1.join(','),
+      action: arr2.join(','),
+    });
     MessagePlugin.success('修改成功');
     await fetchData();
     editVisible.value = false;
@@ -273,7 +401,6 @@ const deleteByIds = async (ids) => {
 const handleRowEdit = ({ row }) => {
   isEdit.value = true;
   showEdit(row);
-  console.log(row);
 };
 
 const handleRowDelete = ({ row }) => {
@@ -306,9 +433,111 @@ const handleSubmit = async ({ validateResult }) => {
 };
 
 const handlePageChange = (curr) => {
-  pagination.value.defaultCurrent = curr.current;
-  pagination.value.defaultPageSize = curr.pageSize;
+  pagination.value.current = curr.current;
+  pagination.value.pageSize = curr.pageSize;
   fetchData();
+};
+
+const selectUserOptions = ref([]);
+const selectUserInput = ref('');
+const selectUserData = ref([]);
+const selectUserChecked = ref([]);
+
+const selectUserCheckedChange = () => {
+  const arr = [];
+  const list = selectUserData.value;
+  for (let i = 0, len = list.length; i < len; i++) {
+    arr.push(list[i].value);
+  }
+  selectUserChecked.value = arr;
+  selectUserInput.value = '';
+  fetchUserData();
+};
+
+const selectUserOptionsCheckedChange = (val, { current, type }) => {
+  if (!current) {
+    selectUserData.value = type === 'check' ? selectUserData.value.slice(1) : [];
+    return;
+  }
+  if (type === 'check') {
+    const option = selectUserOptions.value.find((t) => t.value === current);
+    selectUserData.value.push(option);
+  } else {
+    selectUserData.value = selectUserData.value.filter((v) => v.value !== current);
+  }
+  selectUserCheckedChange();
+};
+
+const selectUserTagChange = (currentTags, context) => {
+  const { trigger, index, item } = context;
+  if (trigger === 'clear') {
+    selectUserData.value = [];
+  }
+  if (['tag-remove'].includes(trigger)) {
+    selectUserData.value.splice(index, 1);
+  }
+  if (trigger === 'enter') {
+    const current = { label: item, value: item };
+    selectUserData.value.push(current);
+    selectUserOptions.value = selectUserOptions.value.concat(current);
+    selectUserInput.value = '';
+  }
+  selectUserCheckedChange();
+};
+
+const selectUserInputChange = () => {
+  fetchUserData();
+};
+
+const selectActionOptions = ref([]);
+const selectActionInput = ref('');
+const selectActionData = ref([]);
+const selectActionChecked = ref([]);
+
+const selectActionCheckedChange = () => {
+  const arr = [];
+  const list = selectActionData.value;
+  for (let i = 0, len = list.length; i < len; i++) {
+    arr.push(list[i].value);
+  }
+  selectActionChecked.value = arr;
+  selectActionInput.value = '';
+  fetchActionData();
+};
+
+const selectActionOptionsCheckedChange = (val, { current, type }) => {
+  if (!current) {
+    selectActionData.value = type === 'check' ? selectActionData.value.slice(1) : [];
+    return;
+  }
+  if (type === 'check') {
+    const option = selectActionOptions.value.find((t) => t.value === current);
+    selectActionData.value.push(option);
+  } else {
+    selectActionData.value = selectActionData.value.filter((v) => v.value !== current);
+  }
+  selectActionCheckedChange();
+};
+
+const selectActionTagChange = (currentTags, context) => {
+  const { trigger, index, item } = context;
+  if (trigger === 'clear') {
+    selectActionData.value = [];
+  }
+  if (['tag-remove'].includes(trigger)) {
+    selectActionData.value.splice(index, 1);
+  }
+  if (trigger === 'enter') {
+    const current = { label: item, value: item };
+    selectActionData.value.push(current);
+    selectActionOptions.value = selectActionOptions.value.concat(current);
+    selectActionInput.value = '';
+  }
+  selectActionCheckedChange();
+};
+
+const selectActionInputChange = () => {
+  fetchActionData();
 };
 
 onMounted(() => {
